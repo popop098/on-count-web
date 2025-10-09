09d3e610d9dad254cc8f46afe0fa4bdc9fd737cd
unknown <popop098@outlook.kr>
2025-10-09 09:56:32 +0900

    add

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,18 @@
 import { Head, Html, Main, NextScript } from "next/document";
-
+import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+            <Script async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2779482138205092"
+                    crossOrigin="anonymous"></Script>
+            <meta name="google-adsense-account" content="ca-pub-2779482138205092"/>
+        </Head>
+        <body className="antialiased">
+        <Main/>
+        <NextScript/>
+            </body>
     </Html>
-  );
+);
 }
